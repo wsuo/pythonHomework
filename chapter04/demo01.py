3f292e224648297240d882d0c4d702e5d7558881
wsuo <ws2821@yeah.net>
2020-03-16 22:15:59 +0800

    完成身份证核验的作业

---
+++ chapter04/demo01.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/3/16 0016 18:26
+# @Author  : 王硕
+# @Email   : ws2821@yeah.net
+# @File    : demo01.py
+# @Software: PyCharm
+'''
+一个合法的身份证号码由17位地区、日期编号和顺序编号加1位校验码组成。校验码的计算规则如下：
+
+首先对前17位数字加权求和，权重分配为：{7，9，10，5，8，4，2，1，6，3，7，9，10，5，8，4，2}；
+
+然后将计算的和对11取模得到值Z；最后按照以下关系对应Z值与校验码M的值：
+
+Z：0 1 2 3 4 5 6 7 8 9 10
+
+M：1 0 X 9 8 7 6 5 4 3 2
+
+1、先把集合反转了:
+10 9 8 7 6 5 4 3 2 1 0
+2、然后加2:
+12 11 10 9 8 7 6 5 4 3 2
+3、从 9 往后是一样的，再将前三个变为12,11,10
+得到的集合即为结果
+
+
+输入一个身份证号码，请你验证校验码的有效性，给出结论是否为合法身份证号码
+
+输入样例：
+
+320124198808240056
+
+输出样例：
+
+320124198808240056是合法身份证号
+'''
+
+
+# 运算函数
+def decide(m):
+    # sum = 0
+    # 在这里定义的原因是考虑到如果程序都运行不到这一步会浪费空间
+    lt = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2]
+    # for i in range(17):
+    # 对前17位数字加权求和
+    # tmp = int(m[i]) * int(lt[i])
+    # sum += tmp
+    # 使用列表推导式更为简单
+    mod = sum([int(m[i]) * int(lt[i]) for i in range(17)]) % 11
+    # 取模判断
+    nav = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    rev = [1, 0, 'X', 9, 8, 7, 6, 5, 4, 3, 2]
+    # if [i for i in nav if mod == nav[i]][0]
+    for j in nav:
+        if mod == nav[j]:
+            return rev[j]
+    return
+
+
+# 判断函数
+def doif(m, tFlag):
+    if tFlag:
+        print("{:s}是合法身份证号".format(m))
+    else:
+        print("{:s}不是合法身份证号".format(m))
+
+
+# 主函数
+m = input("请输入身份证号:")
+# 首先判断长度是不是18位
+totalFlag = False
+if len(m) == 18:
+    # 遍历一下看看是不是都是数字
+    flag = True
+    for i in range(17):
+        if not m[i].isdigit():
+            flag = False
+    if flag:
+        # 如果过了这两关，往下走判断
+        tail = decide(m)
+        if tail == int(m[-1]):
+            totalFlag = True
+doif(m, totalFlag)
+#     else:
+#         totalFlag = False
+# else:
+#     totalFlag = False
+
+# print("请输入正确的身份证号!(不全是数字)")
+# print("请输入正确的身份证号!(长度不对)")
---
+++ chapter04/demo02.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/3/16 0016 20:18
+# @Author  : 王硕
+# @Email   : ws2821@yeah.net
+# @File    : demo02.py
+# @Software: PyCharm
+'''
+随机密码生成。编写程序，在26个大、小写字母和10个数字组成的列表中随机生成1个密码，密码长度8位。
+'''
+import random as r
+
+n = eval(input("请输入生成密码的位数:"))
+ch = ''
+for i in range(n):
+    up = r.randint(65, 90)
+    low = r.randint(97, 122)
+    dic = r.randint(48, 57)
+    tmp = r.randint(1, 3)
+    if tmp == 1:
+        ch += chr(up)
+    elif tmp == 2:
+        ch += chr(low)
+    else:
+        ch += chr(dic)
+print(ch)
